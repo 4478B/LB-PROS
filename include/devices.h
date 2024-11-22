4ce004f7669e50ef5e2ce3db74711ea6030de62c
Evan Boyle <evan.wesley.boyle@gmail.com>
2024-11-22 08:40:23 -0500

    working on odom & figuring our pure pursuit

---
+++ include/devices.h
@@ -0,0 +1,31 @@
+#ifndef DEVICES_H
+#define DEVICES_H
+
+// required files for devices
+#include "main.h"
+#include "lemlib/api.hpp"
+#include "lemlib/pid.hpp"
+
+// namespace for declarations
+using namespace pros;
+using namespace lemlib;
+
+// External variable declarations
+extern MotorGroup left_motors;
+extern MotorGroup right_motors;
+extern MotorGroup arm_motors;
+extern Controller controller;
+extern Motor intake;
+extern adi::Port clamp;
+
+extern PID armPID;
+extern Rotation armRot;
+extern Drivetrain drivetrain;
+extern Imu imu;
+extern OdomSensors sensors;
+extern ControllerSettings lateral_controller;
+extern ControllerSettings angular_controller;
+extern ExpoDriveCurve throttle_curve;
+extern Chassis chassis;
+
+#endif // DEVICES_H
--- src/main.cpp
+++ src/main.cpp
@@ -1,91 +1,15 @@
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
 #include "lemlib/pid.hpp"
+#include "liblvgl/llemu.hpp"
 #include "pros/adi.h"
 #include "pros/misc.h"
+#include "pros/motors.h"
 #include "pros/rotation.hpp"
 #include <cstdlib>
-
-using namespace pros;
-using namespace lemlib;
+#include "devices.h"
 
 
-// left motor group
-MotorGroup left_motors({-8, -9, -10}, pros::MotorGearset::blue);
-// right motor group
-MotorGroup right_motors({5, 6, 7}, pros::MotorGearset::blue);
-
-MotorGroup arm_motors({-3, 4}, pros::MotorGearset::blue);
-
-// controller definition
-Controller controller(pros::E_CONTROLLER_MASTER);
-
-Motor intake(1);
-
-adi::Port clamp('B', pros::E_ADI_DIGITAL_OUT);
-
-PID armPID(1, 0, 2);
-
-Rotation armRot(2);
-
-bool clampState = true;
-
-
-// drivetrain settings
-lemlib::Drivetrain drivetrain(&left_motors, // left motor group
-                              &right_motors, // right motor group
-                              11, // 11 inch track width
-                              lemlib::Omniwheel::NEW_275, // using new 2.75" omnis
-                              450, // drivetrain rpm is 360
-                              2 // horizontal drift is 2 (for now)
-);
-
-pros::Imu imu(20);
-
-lemlib::OdomSensors sensors(nullptr, // vertical tracking wheel 1, set to null
-                            nullptr, // vertical tracking wheel 2, set to nullptr as we are using IMEs
-                            nullptr, // horizontal tracking wheel 1
-                            nullptr, // horizontal tracking wheel 2, set to nullptr as we don't have a second one
-                            &imu // inertial sensor
-);
-
-// lateral PID controller
-lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              3, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in inches
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in inches
-                                              500, // large error range timeout, in milliseconds
-                                              20 // maximum acceleration (slew)
-);
-
-// angular PID controller
-lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
-                                              0, // integral gain (kI)
-                                              10, // derivative gain (kD)
-                                              3, // anti windup
-                                              1, // small error range, in inches
-                                              100, // small error range timeout, in milliseconds
-                                              3, // large error range, in inches
-                                              500, // large error range timeout, in milliseconds
-                                              0 // maximum acceleration (slew)
-);
-
-// input curve for throttle input during driver control
-lemlib::ExpoDriveCurve throttle_curve(3, // joystick deadband out of 127
-                                     10, // minimum output where drivetrain will move out of 127
-                                     1.019 // expo curve gain
-);
-
-// create the chassis
-lemlib::Chassis chassis(drivetrain, // drivetrain settings
-                        lateral_controller, // lateral PID settings
-                        angular_controller, // angular PID settings
-                        sensors, // odometry sensors
-						&throttle_curve // log drive
-);
 
 /**
  * A callback function for LLEMU's center button.
@@ -106,15 +30,20 @@ void on_center_button() {
 
 // initialize function. Runs on program startup
 void initialize() {
-    pros::lcd::initialize(); // initialize brain screen
+    lcd::initialize(); // initialize brain screen
     chassis.calibrate(); // calibrate sensors
 
-    armRot.reset_position();
+    
+    // configure motors for arm movement
+    armRot.reset_position(); 
+    arm_motors.set_brake_mode(E_MOTOR_BRAKE_HOLD);
 
-	pros::lcd::initialize();
-	pros::lcd::set_text(1, "Hello PROS User!");
+    // assign buttons to actions in auton selector
+	lcd::register_btn1_cb(on_center_button);
+    lcd::register_btn0_cb(on_center_button);
 
-	pros::lcd::register_btn1_cb(on_center_button);
+
+    pros::lcd::set_text_align(pros::lcd::Text_Align::CENTER);
 	
     // print position to brain screen
     pros::Task screen_task([&]() {
@@ -123,7 +52,6 @@ void initialize() {
             pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
             pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
             pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-            pros::lcd::print(7,"Arm Rotation: %f", armRot.get_angle()/100.0); // arm position
 
             // delay to save resources
             pros::delay(20);
@@ -226,14 +154,31 @@ void testLateralPID(){
  * will be stopped. Re-enabling the robot will restart the task, not re-start it
  * from where it left off.
  */
- ASSET(path_jerryio_txt);
+ ASSET(test4_txt);
 void autonomous() {
     // set position to x:0, y:0, heading:0
     chassis.setPose(0, 0, 0);
     // turn to face heading 90 with a very long timeout
     
-	chassis.follow(path_jerryio_txt, 15, 2000);
-	
+    chassis.moveToPose(63,-63,50,20000);
+    
+    
+    
+    
+    
+    
+    
+    /*controller.set_text(1,1,"Started I <3 Mikey");
+	chassis.follow(test4_txt, 20, 20000,true,false);
+    intake.move(127);
+    chassis.setBrakeMode(pros::E_MOTOR_BRAKE_HOLD);
+    chassis.cancelAllMotions();
+    controller.clear_line(1);
+    controller.set_text(1,1,"Stopped");*/
+    std::cout<<"Done";
+	while(true){
+        delay(20);
+    }
 
 }
 
@@ -264,7 +209,7 @@ void handleIntake(){
 
 }
 
-    bool clampCD = false;
+bool clampState = false;
 
 void handleClamp(){
 
@@ -287,24 +232,24 @@ void handleClamp(){
 double error = 0;
 double targetPos = 0, currentPos = 0;
 double nextMovement = 0;
-double armThreshold = 1; // threshold for goal to be met
+double armThreshold = 2; // threshold for goal to be met
 bool armMoving = false;
 
 void handleArm(){
 
 
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN))
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L1))
     {
-        targetPos = 10; // bottom position; the starting value in code
+        targetPos = 0; // bottom position; the starting value in code
         armMoving = true;
     }
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_LEFT))
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_L2))
     {
-        targetPos = 35; // middle position; grabs from intake 
+        targetPos = 27; // middle position; grabs from intake 
         armMoving = true;
     }
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)){
-        targetPos = 140; // top position; scoring on wall stake
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN)){
+        targetPos = 130; // top position; scoring on wall stake
         armMoving = true;
     }
 
@@ -314,12 +259,8 @@ void handleArm(){
         currentPos = armRot.get_angle()/100.0;
         
         // normalize error to [-180,180]
-        if(currentPos > 180){
-            currentPos -= 360;
-        }
-        else if(currentPos < -180){
-            currentPos += 360;
-        }
+        currentPos = currentPos - 360 * (currentPos > 180)
+                                + 360 * (currentPos < -180);
 
 
         // calculate how far arm is from target
@@ -359,6 +300,16 @@ void handleArm(){
     }
      
 }
+
+/*int countPoints = 1;
+
+void handleSavePoint(){
+    if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_A)){
+        std::cout<<"Point " + countPoints + ": " + chassis.getPose();
+
+    }
+
+}*/
 
 
 
